2caf37c927c0843a86083f78514f2d5abd3dcfed
hank <anima923@hotmail.com>
2022-02-28 21:47:04 +0800

    圖表增加長條圖

--- js/chart.js
+++ js/chart.js
@@ -1,6 +1,108 @@
 
-function showScatter() {
 
+
+function showChart(){
+   let type =$("#chartType").val();
+   console.log(type);
+   switch(type){
+      case "scatter":
+         showScatter();
+         break;
+      case "bar":
+         showBar();
+         break;
+   }
+}
+
+function showBar(){
+   $("#container").css("height","600px");
+   var chart = {
+      type: 'bar'
+   };
+   var title = {
+      text: 'Historic World Population by Region'   
+   };
+   var subtitle = {
+      text: 'Source: Wikipedia.org'  
+   };
+   var yAxis = {
+      min: 0,
+      title: {
+         text: '金額',
+         align: 'high'
+      },
+      labels: {
+         overflow: 'justify'
+      }
+   };
+   var tooltip = {
+      valueSuffix: ' millions'
+   };
+   var plotOptions = {
+      bar: {
+         dataLabels: {
+            enabled: true
+         }
+      }
+   };
+   var legend = {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'top',
+      x: -40,
+      y: 100,
+      floating: true,
+      borderWidth: 1,
+      backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+      shadow: true
+   };
+   var credits = {
+      enabled: false
+   };
+   let nameArr = new Array();
+   let nameAmounts = new Array();
+   let nameTotal = new Array();
+   originData.list.forEach( e =>{
+      nameArr.push(e.name);
+      nameAmounts.push(e.amounts);
+      nameTotal.push(e.totalAmounts);
+   })
+
+   var xAxis = {
+      categories: nameArr,
+      title: {
+         text: null
+      }
+   };
+   var series= [{
+         name: 'amounts',
+            data: nameAmounts
+        }, {
+            name: 'totalAmounts',
+            data: nameTotal
+        }
+   ];     
+      
+   var json = {};   
+   json.chart = chart; 
+   json.title = title;   
+   json.subtitle = subtitle; 
+   json.tooltip = tooltip;
+   json.xAxis = xAxis;
+   json.yAxis = yAxis;  
+   json.series = series;
+   json.plotOptions = plotOptions;
+   json.legend = legend;
+   json.credits = credits;
+   $('#container').highcharts(json);
+  
+}
+
+
+
+function showScatter() {
+   
+   $("#container").css("height","480px");
     var chart = {
         type: 'scatter',
         zoomType: 'xy'
@@ -83,14 +185,4 @@ function showScatter() {
    json.plotOptions = plotOptions;
    $('#container').highcharts(json);
 
-}
-
-function showChart(){
-   let type =$("#chartType").val();
-   console.log(type);
-   switch(type){
-      case "scatter":
-         showScatter();
-         break;
-   }
 }
